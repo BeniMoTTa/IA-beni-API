25c9dec3805d0d3e716ad66009ed1fe53026d512
Jozhué Beni <jbeeniif@gmail.com>
2023-09-13 10:05:08 -0300

    Aside almost finished

--- ai-web-beni/src/App.tsx
+++ ai-web-beni/src/App.tsx
@@ -1,7 +1,8 @@
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
-import { Github } from "lucide-react";
+import { Github, FileVideo, Upload } from "lucide-react";
 import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
@@ -39,9 +40,10 @@ export function App() {
           <form className="space-y-6">
             <label
               htmlFor="video"
-              className="border flex rounded-md aspect-video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
             >
-              Carregar Vídeo
+              <FileVideo className="w-4 h-4" />
+              Selecione um Vídeo
             </label>
             <input
               type="file"
@@ -49,6 +51,27 @@ export function App() {
               accept="video/mp4"
               className="sr-only"
             />
+
+            <Separator />
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">
+                {" "}
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className=" w-4 h-4 ml-2" />
+            </Button>
+          </form>
+          <Separator />
+          <form className="space-y-6">
+            <div className=""></div>
           </form>
         </aside>
       </main>
