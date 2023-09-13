11a58addfa0644b718857b8a9944622d13b92401
Jozhué Beni <jbeeniif@gmail.com>
2023-09-13 14:48:10 -0300

    post upload testing

--- ai-back-beni/src/server.ts
+++ ai-back-beni/src/server.ts
@@ -1,11 +1,12 @@
 import { fastify } from "fastify";
 import { prisma } from "./lib/prisma";
 import { getAllPromptsRoute } from "./routes/getAllPrompts";
+import { uploadVideoRoute } from "./routes/uploadVideos";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
-
+app.register(uploadVideoRoute);
 app
   .listen({
     port: 3333,
